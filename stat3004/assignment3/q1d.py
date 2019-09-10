f582481b96f1b39c87736949acefa04bf2753c79
Joshua <joshua.hwang@ieee.org>
2019-09-10 13:22:11 +1000

    dump

--- stat3004/assignment3/q1d.py
+++ stat3004/assignment3/q1d.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
+import math
 import random
+random.seed(44302650) # My student number
 
 N = 10**3
 
@@ -38,20 +40,27 @@ res = [step(step(initial())) for i in range(N)]
 print("==================================================")
 # first part
 pi = sum(((X == 1) for X in res))/N
-print("P(X2=1) ~ {}".format(pi))
-print("P(X2=1) = {}".format(17/36))
+pise = math.sqrt(1/(N-1)*sum(((X == 1) - pi)**2 for X in res))/math.sqrt(N)
+#pise = math.sqrt(pi*(1-pi)/(N-1))/math.sqrt(N)
+print("P(X_2=1) ~ {}".format(pi))
+print("standard error = {}".format(pise))
+print("P(X_2=1) = {}".format(17/36))
 
 print("--------------------------------------------------")
 
 # second part
 mu = sum((X for X in res))/N
-print("E[X2] ~ {}".format(mu))
-print("E[X2] = {}".format(71/216))
+muse = math.sqrt(sum((X - mu)**2 for X in res)/(N-1))/math.sqrt(N)
+print("E[X_2] ~ {}".format(mu))
+print("standard error = {}".format(muse))
+print("E[X_2] = {}".format(71/216))
 
 print("--------------------------------------------------")
 
 # third part
 s2 = sum((X - mu)**2 for X in res)/(N-1)
-print("Var(X2) ~ {}".format(s2))
-print("Var(X2) = {}".format(23687/46656))
+s2se = math.sqrt(sum(((X - mu)**2 - s2)**2 for X in res)/(N-1))/math.sqrt(N)
+print("Var(X_2) ~ {}".format(s2))
+print("standard error = {}".format(s2se))
+print("Var(X_2) = {}".format(23687/46656))
 print("==================================================")
